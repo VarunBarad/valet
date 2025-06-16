9cb67cde6b206dd1eb47ad03952a21fb2370f1a4
Varun Barad <varun@varunbarad.com>
2025-06-16 12:17:55 +0530

    Fix typo in build's error message.

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
         "cargo:rustc-env=CREDIT_CARD_HDFC_PASSWORD={}",
         credit_card_hdfc_password
     );
-    let credit_card_icici_password = std::env::var("CREDIT_CARD_ICICI_PASSWORD").expect("Please provide password to use for opening HDFC Bank Credit Card statements using environment variable \"CREDIT_CARD_ICICI_PASSWORD\"");
+    let credit_card_icici_password = std::env::var("CREDIT_CARD_ICICI_PASSWORD").expect("Please provide password to use for opening ICICI Bank Credit Card statements using environment variable \"CREDIT_CARD_ICICI_PASSWORD\"");
     println!(
         "cargo:rustc-env=CREDIT_CARD_ICICI_PASSWORD={}",
         credit_card_icici_password
